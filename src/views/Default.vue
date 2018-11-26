c02da7d033624452385d7f2589d53769973de7c8
Johannes Raggam <thetetet@gmail.com>
2018-11-26 22:09:00 +0100

    list all navigatable items

--- src/views/Default.vue
+++ src/views/Default.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="viewWrapper">
     <h1>{{ context.title }}</h1>
+
+    <nav>
+      <ul>
+        <li v-for="item in context.items">
+          <router-link :to="{ name: 'view-factory' }">{{ item.title }}</router-link>
+        </li>
+      </ul>
+
+    </nav>
+
   </div>
 </template>
 <script>
