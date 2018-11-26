1c9f17747dee6d40ea1f4ff469b46b5a74b6141f
Johannes Raggam <thetetet@gmail.com>
2018-11-27 00:03:58 +0100

    nav = component

---
+++ src/components/foldernavigation.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <ul>
+      <li v-if="parentPath">
+        <router-link :to="{ path: parentPath }">Parent Item</router-link>
+      </li>
+      <li v-for="item in context.items">
+        <router-link :to="{ path: makePath(item['@id']) }">{{ item.title }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+import utils from '@/utils';
+
+export default {
+  computed: {
+    context () {
+      return this.$store.state.context;
+    },
+    parentPath () {
+      // Create path to parent directory
+      let path = this.$route.path.split('/');
+      if (this.$route.path === '/' || path.length < 2) {
+        return null;
+      }
+      path.splice(-1, 1);
+      return path.join('/') || '/';
+    }
+  },
+  methods: {
+    makePath (uri) {
+      return utils.makePath(uri);
+    },
+  }
+}
+</script>
--- src/views/Default.vue
+++ src/views/Default.vue
@@ -1,41 +1,16 @@
 <template>
   <div class="viewWrapper">
     <h1>{{ context.title }}</h1>
-
-    <nav>
-      <ul>
-        <li v-if="parentPath">
-          <router-link :to="{ path: parentPath }">Parent Item</router-link>
-        </li>
-        <li v-for="item in context.items">
-          <router-link :to="{ path: makePath(item['@id']) }">{{ item.title }}</router-link>
-        </li>
-      </ul>
-
-    </nav>
-
+    <FolderNavigation/>
   </div>
 </template>
 <script>
-import utils from '@/utils';
+import FolderNavigation from '@/components/foldernavigation';
 
 export default {
-  props: ['context'],
-  computed: {
-    parentPath () {
-      // Create path to parent directory
-      let path = this.$route.path.split('/');
-      if (this.$route.path === '/' || path.length < 2) {
-        return null;
-      }
-      path.splice(-1, 1);
-      return path.join('/') || '/';
-    }
+  components: {
+    FolderNavigation,
   },
-  methods: {
-    makePath (uri) {
-      return utils.makePath(uri);
-    },
-  }
+  props: ['context'],
 }
 </script>
