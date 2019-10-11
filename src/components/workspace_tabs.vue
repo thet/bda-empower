29c3b31425ef18ae645f8ff4e87ea8d833503ba6
Johannes Raggam <thetetet@gmail.com>
2019-10-11 17:17:49 +0200

    use router-link. a-hrefs in vue/router based applications do force reloads and do not work well

--- src/components/workspace_tabs.vue
+++ src/components/workspace_tabs.vue
@@ -1,11 +1,11 @@
 <template>
 
   <nav class="em-workspace-tabs" :class="`em-workspace-tabs-${current_workspace}`">
-    <a class="em-btn"
+    <router-link class="em-btn"
       v-for="ws of workspaces"
       :key="`workspace-tab-${ws.id}`"
-      :class="[`em-btn-${ws.id}`, {active: current_workspace == ws.id}]"
-      :href="`${base_url}/${ws.id}`">{{ ws.title }}</a>
+      :to="{ path: ws_path(ws.id)  }"
+      :class="[`em-btn-${ws.id}`, {active: current_workspace === ws.id}]">{{ ws.title }}</router-link>
   </nav>
 
 </template>
@@ -18,9 +18,6 @@ export default {
   computed: {
     current_workspace() {
       return this.$route.params.workspace || this.current_context.workspace;
-    },
-    base_url() {
-      return `${utils.makePath(this.current_case['@id'])}/+ws`;
     },
     workspaces() {
       return [
@@ -35,6 +32,12 @@ export default {
       current_case: state => state.context.current_case,
       current_context: state => state.context.current_context
     })
+  },
+
+  methods: {
+    ws_path(ws_id) {
+      return `${utils.makePath(this.current_case['@id'])}/+ws/${ws_id}`;
+    },
   }
 
 };
